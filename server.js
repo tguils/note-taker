bfa32dee772cdb0bc5d7e65d105b4f80ad8db3c0
tguils <tess.guilmette@gmail.com>
2023-07-29 11:26:34 -0400

    joining html files to the routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -10,3 +11,13 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+// joining index.html on the route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
+  // joining notes.html on the  route
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'notes.html'));
+  });
