c769489e0b17178ab33b6fe5d2ed5b03e491e64b
tguils <tess.guilmette@gmail.com>
2023-07-29 11:43:31 -0400

    to do list for routes and functions

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
+https://expressjs.com/en/resources/middleware/body-parser.html
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
@@ -21,3 +24,10 @@ app.get('/', (req, res) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
   });
+
+  //to do:
+  //if notes exist - load them
+  //save notes - stringify?
+  // retrieve all notes from api
+  // create route for new notes
+  // delete notes function
